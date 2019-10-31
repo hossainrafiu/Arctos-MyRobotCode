7c71dafe4aad84bef31e50ee176684c31bcd87e8
Rafiu Hossain <hossainrafiu@gmail.com>
2019-10-31 19:15:21 -0400

    Small Changes from Last commit
    Got rid of doTask from Robot; Added comments throughout the classes and
    code that I implemented

--- src/com/arctos6135/training/quiz/robot/MainRobot.java
+++ src/com/arctos6135/training/quiz/robot/MainRobot.java
@@ -5,32 +5,50 @@ import com.arctos6135.training.quiz.robot.actuators.*;
 
 public class MainRobot extends Robot{
 
-    Encoder leftWheel = new Encoder(2);
-    Encoder rightWheel = new Encoder(3);
-    Gyroscope gyro = new Gyroscope(4);
-    Motor leftMotor = new Motor(0);
-    Motor rightMotor = new Motor(1);
+    // Defining the various devices on the robot and which port they are connected to
+    public Encoder leftWheel = new Encoder(2);
+    public Encoder rightWheel = new Encoder(3);
+    public Gyroscope gyro = new Gyroscope(4);
+    public Motor leftMotor = new Motor(0);
+    public Motor rightMotor = new Motor(1);
     private State state = State.UNKNOWN;
 
+    public MainRobot(){
+    }
+
+    public MainRobot(String name){
+        setName(name);
+    }
+
+    public MainRobot(String name, State state){
+        writeState(state);
+        setName(name);
+    }
+
+    // Getting the distance the left wheel has travelled using the leftWheel encoder
     @Override
     public double getLeftWheelDistance(){
         return leftWheel.getDistance()/2.54;
     }
 
+    // Getting the distance the right wheel has travelled using the rightWheel encoder
     @Override
     public double getRightWheelDistance(){
         return rightWheel.getDistance()/2.54;
     }
 
+    // Getting the heading of the robot using the gyroscope and getting a value between 0 and 360
     @Override
     public double getHeading(){
         return gyro.getHeading()%360;
     }
 
+    // Returns the state of the Robot
     public State readState(){
         return state;
     }
 
+    // Sets the State of the Robot
     public void writeState(State state){
         this.state = state;
     }
--- src/com/arctos6135/training/quiz/robot/RobotTester.java
+++ src/com/arctos6135/training/quiz/robot/RobotTester.java
@@ -11,17 +11,30 @@ public class RobotTester{
         }
     }
     public static void main(String[] args) {
+        
         MainRobot robot1 = new MainRobot();
+        
+        // Calling the static doSomething function from above
         doSomething(robot1);
+
+        // Changing robot1's state to ENABLED
         robot1.writeState(State.ENABLED);
-        robot1.doTask(robot -> {
-            if(robot.readState() == State.ENABLED){
-                robot.leftMotor.setSpeed(1.0);
-                robot.rightMotor.setSpeed(-1.0);
+        
+        // Here, we override the doIt method from the Task interface
+        robot1.doTask(robot -> 
+            {
+                
+                if(robot.readState() == State.ENABLED)
+                {
+                    robot.leftMotor.setSpeed(1.0);
+                    robot.rightMotor.setSpeed(-1.0);
+                }
+                else if(robot.readState() == State.UNKNOWN)
+                {
+                    throw new TaskExecutionException("The State of the Robot is UNKNONW!");
+                }
+
             }
-            else if(robot.readState() == State.UNKNOWN){
-                throw new TaskExecutionException("The State of the Robot is UNKNONW!");
-            }
-        });
+        );
     }
 }
